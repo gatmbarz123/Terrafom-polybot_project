916c333ac7af120f9c5bed6947eff594adceb650
gatmbarz123 <bar30301@gmail.com>
2024-10-22 12:12:04 +0300

    Check the CI

--- infrastructure/module/polybot/main.tf
+++ infrastructure/module/polybot/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "polybot" {
-  count = 2
+  count = 3
   ami           = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.polybot_sg.id]
