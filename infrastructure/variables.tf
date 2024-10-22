3f3df8c0d1773be4ccb836a8037e60fdace78247
gatmbarz123 <bar30301@gmail.com>
2024-10-22 12:35:13 +0300

    Check the CI

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,31 +1,3 @@
-variable "instance_type" {
-   description = "instance_type"
-   type        = string
-}
-variable "key_pairs" {
-   description = "key_name"
-   type        = string
-}
-variable "s3_name" {
-   description = "s3_name"
-   type        = string
-}
-variable "dynamodb_name" {
-   description = "dynamodb_name"
-   type        = string
-}
-
-variable "sqs_name" {
-   description = "sqs_name"
-   type        = string
-}
-
-variable "record_name" {
-  description = "The record name"
-  type        = string
-
-}
-
 variable "certificate_arn" {
   description = "The certificate arn"
   type        = string
