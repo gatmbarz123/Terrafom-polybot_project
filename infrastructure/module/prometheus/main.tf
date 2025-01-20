decc30193412938d2a5a765ce9cb01b300c82241
gatmbarz123 <bar30301@gmail.com>
2025-01-20 15:23:00 +0200

    add prometheus + grafana

--- infrastructure/module/prometheus/main.tf
+++ infrastructure/module/prometheus/main.tf
@@ -14,16 +14,15 @@ resource "aws_instance" "prometheus" {
 
     provisioner "remote-exec" {
       inline = [
-
         "sudo apt update -y",
         "sudo apt install -y docker.io",
         "sudo systemctl start docker",
         "sudo systemctl enable docker",
         "sudo usermod -aG docker $USER",
-        "sudo curl -SL https://github.com/docker/compose/releases/download/v2.34.4/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose",
+        
+        "sudo curl -L \"https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
         "sudo chmod +x /usr/local/bin/docker-compose",
-        "sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose",
-            
+        "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose",
 
         "sudo mkdir -p /etc/prometheus",
         "sudo mkdir -p /var/lib/prometheus",
